483504e8ae6d23977df4a831443d793b993c2615
OD3WO <teerasak.dev@mail.kmutt.ac.th>
2023-09-10 17:08:13 +0700

    Match password UI

--- src/views/usermanage/PasswordMatch.vue
+++ src/views/usermanage/PasswordMatch.vue
@@ -12,15 +12,28 @@ const User = ref({
 
 const check = async () => {
   let result = await Authenfund(User.value);
-  if (result == true) {
+  if (result == 200) {
     Swal.fire({
       icon: "success",
       title: "PASSWORD IS MATCH",
       confirmButtonText: "Continue",
       customClass: {
         container: 'your-custom-class',
-        title: 'your-title-class',
+        title: 'font-noto ann-message',
         content: 'your-content-class',
+        confirmButton: 'your-confirm-button-class',
+        cancelButton: 'font-noto',
+      },
+    });
+  } else if(result === 404) {
+    Swal.fire({
+      icon: "error",
+      title: "PASSWORD IS NOT MATCH",
+      confirmButtonText: "Continue",
+      customClass: {
+        container: 'your-custom-class',
+        title: 'font-noto text-red-500 ann-message',
+        content: 'text-white',
         confirmButton: 'your-confirm-button-class',
         cancelButton: 'font-noto',
       },
@@ -28,11 +41,11 @@ const check = async () => {
   } else {
     Swal.fire({
       icon: "error",
-      title: "PASSWORD IS NOT MATCH",
+      title: "THE SPECIFIED USERNAME DOES NOT EXIST",
       confirmButtonText: "Continue",
       customClass: {
         container: 'your-custom-class',
-        title: 'font-noto text-red-500',
+        title: 'font-noto text-red-500 ann-message',
         content: 'text-white',
         confirmButton: 'your-confirm-button-class',
         cancelButton: 'font-noto',
@@ -62,83 +75,34 @@ const check = async () => {
       <!-- content  -->
       <div class="w-full flex h-5/6 bg-white shadow-md rounded-2xl">
         <div class="w-full flex flex-col justify-center items-center space-y-10">
-          <h1 class="text-4xl font-extrabold ann-title text-custom-black">Match Password</h1>
-          <div class="w-1/3 flex flex-col space-y-10">
-            <div class="w-full flex flex-row space-x-10">
-              <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 512 512">
+          <div class="w-1/3 flex flex-col space-y-10 shadow-equal-shadow p-10 rounded-xl">
+            <h1 class="text-4xl font-extrabold ann-title text-center text-custom-black">Match Password</h1>
+            <div class="w-full flex flex-row space-x-6 items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 512 512">
                 <path fill="#58abe5"
                   d="m448 362.7l-117.3-21.3C320 320 320 310.7 320 298.7c10.7-10.7 32-21.3 32-32c10.7-32 10.7-53.3 10.7-53.3c5.5-8 21.3-21.3 21.3-42.7s-21.3-42.7-21.3-53.3C362.7 32 319.2 0 256 0c-60.5 0-106.7 32-106.7 117.3c0 10.7-21.3 32-21.3 53.3s15.2 35.4 21.3 42.7c0 0 0 21.3 10.7 53.3c0 10.7 21.3 21.3 32 32c0 10.7 0 21.3-10.7 42.7L64 362.7C21.3 373.3 0 448 0 512h512c0-64-21.3-138.7-64-149.3z" />
               </svg>
               <input v-model="User.username" type="text"
-                class="bg-gray-50 border border-gray-300 text-custom-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="w-full border-x-0 border-t-0 border-b border-gray-400 text-custom-black focus:ring-0 text-base focus:border-b-2"
                 placeholder="Username" />
             </div>
-            <div class="w-full flex flex-row space-x-10">
-              <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24">
+            <div class="w-full flex flex-row space-x-6 items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
                 <path fill="#58abe5"
                   d="M7 15q1.25 0 2.125-.875T10 12q0-1.25-.875-2.125T7 9q-1.25 0-2.125.875T4 12q0 1.25.875 2.125T7 15Zm0 3q-2.5 0-4.25-1.75T1 12q0-2.5 1.75-4.25T7 6q2.025 0 3.538 1.15T12.65 10h8.375L23 11.975l-3.5 4L17 14l-2 2l-2-2h-.35q-.625 1.8-2.175 2.9T7 18Z" />
               </svg>
               <input v-model="User.password" type="password"
-                class="bg-gray-50 border border-gray-300 text-custom-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="w-full border-x-0 border-t-0 border-b border-gray-600 text-custom-black focus:ring-0 text-base focus:border-b-2"
                 placeholder="Password" />
             </div>
             <div class="w-full flex items-center justify-center">
               <button @click="check()"
-                class="w-full font-bold text-center text-white text-lg py-6 rounded-2xl border border-dashed border-indigo-500 bg-custom-blue">
-                <span
-                  class="">
+                class="w-full font-bold text-center text-white text-lg py-4 rounded-xl bg-custom-blue transition-colors hover:bg-blue-500 duration-300 ann-button">
                   Match
-                </span>
               </button>
             </div>
           </div>
         </div>
-
-
-        <!-- <div class="w-full h-full flex justify-center items-center">
-          <div class="w-3/5 h-5/6 bg-white rounded-2xl">
-            <div class="mt-5 flex flex-col justify-center items-center w-full">
-              <p class="text-4xl font-extrabold ann-title text-custom-black">
-                Match Password
-              </p>
-            </div>
-            <div class="w-full h-3/6 flex justify-center items-center">
-              <div class="flex-col w-4/5">
-                <div class="relative mb-6">
-                  <div class="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 512 512">
-                      <path fill="#58abe5"
-                        d="m448 362.7l-117.3-21.3C320 320 320 310.7 320 298.7c10.7-10.7 32-21.3 32-32c10.7-32 10.7-53.3 10.7-53.3c5.5-8 21.3-21.3 21.3-42.7s-21.3-42.7-21.3-53.3C362.7 32 319.2 0 256 0c-60.5 0-106.7 32-106.7 117.3c0 10.7-21.3 32-21.3 53.3s15.2 35.4 21.3 42.7c0 0 0 21.3 10.7 53.3c0 10.7 21.3 21.3 32 32c0 10.7 0 21.3-10.7 42.7L64 362.7C21.3 373.3 0 448 0 512h512c0-64-21.3-138.7-64-149.3z" />
-                    </svg>
-                  </div>
-                  <input v-model="User.username" type="text"
-                    class="bg-gray-50 border border-gray-300 text-custom-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Username" />
-                </div>
-                <div class="relative mb-6">
-                  <div class="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24">
-                      <path fill="#58abe5"
-                        d="M7 15q1.25 0 2.125-.875T10 12q0-1.25-.875-2.125T7 9q-1.25 0-2.125.875T4 12q0 1.25.875 2.125T7 15Zm0 3q-2.5 0-4.25-1.75T1 12q0-2.5 1.75-4.25T7 6q2.025 0 3.538 1.15T12.65 10h8.375L23 11.975l-3.5 4L17 14l-2 2l-2-2h-.35q-.625 1.8-2.175 2.9T7 18Z" />
-                    </svg>
-                  </div>
-                  <input v-model="User.password" type="password"
-                    class="bg-gray-50 border border-gray-300 text-custom-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Password" />
-                </div>
-                <div class="w-full flex items-center justify-center">
-                  <button @click="check()"
-                    class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-                    <span
-                      class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                      Match
-                    </span>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> -->
       </div>
     </div>
   </div>
